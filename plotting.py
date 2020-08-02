3f089ccc074c2887a4f796ee34391e52d6797dce
harineemira <pa.harineemira@gmail.com>
2020-08-02 23:13:16 +0530

    Marking Ship

--- plotting.py
+++ plotting.py
@@ -1,26 +1,18 @@
 from PIL import Image, ImageDraw, ImageFont
 Image.MAX_IMAGE_PIXELS = None
 import csv
-# create Image object with the input image
  
 image = Image.open('kayal.png')
-
 draw = ImageDraw.Draw(image)
+font = ImageFont.truetype('Roboto-Bold.ttf', size=10)
+name = 'Ship'
+color = 'rgb(255,255,0)'
 
-font = ImageFont.truetype('Roboto-Bold.ttf', size=45)
- 
 with open('Final.csv', 'r') as readFile:
     reader = csv.reader(readFile)
     next(reader)
     for row in reader:
-        lines=list()
-        lines.append(row)
-        a = str(row[2])
-        b = str(row[3])
-        (x, y) = (str(a), str(b))
-        name = 'Ship'
-        color = 'rgb(255,0,0)' 
-        #rgb(255, 255, 255) white
-        draw.text((str(a), str(b)), name, fill=color, font=font)
+        (x,y)=(int(row[2]),int(row[3]))
+        draw.text((x,y), name, fill=color, font=font)
 
 image.save('greeting_card.png')
